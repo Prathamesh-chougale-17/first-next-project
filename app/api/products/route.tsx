6913a99832554de5f3215c2eb3dcc5fad823563c
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-09-30 20:43:17 +0530

    saving the succesful runned prisma request

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,21 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "milk", price: 10 },
-    { id: 2, name: "Bread", price: 15 },
-    { id: 3, name: "egg", price: 21 },
-  ]);
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+  // return NextResponse.json([
+  //   { id: 1, name: "milk", price: 10 },
+  //   { id: 2, name: "Bread", price: 15 },
+  //   { id: 3, name: "egg", price: 21 },
+  // ]);
+  return NextResponse.json(products);
 }
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
+  const body = await request.json(); //get the body of the request
+  // In real application we will insert data into database
+  //In real world we needed to validate the data
+  //if the data is not valid we will return 400 status code
+  //else return the data with 201 status code
   const Validation = schema.safeParse(body);
   if (!Validation.success)
     return NextResponse.json(Validation.error.errors, { status: 400 });
-  return NextResponse.json(
-    { id: 1, name: body.name, price: body.price },
-    { status: 201 }
-  );
+
+  const newUser = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
+  // return NextResponse.json({ id: 1, name: body.name ,price:body.price}, { status: 201 });
 }
--- app/api/products/schema.ts
+++ app/api/products/schema.ts
@@ -1,9 +1,8 @@
 import { z } from "zod";
 
 const schema = z.object({
-    id: z.number().int().positive(),
-    name: z.string().min(3),
-    price: z.number().min(0),
+    name: z.string(),
+    price: z.number(),
 
 })
 
