a0bdd3d611182880b5d6da0ab95161cd958eeada
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-10-01 16:11:23 +0530

    saving the middleware successfully

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+//this used to protect the routes
+// import { NextRequest, NextResponse } from "next/server";
+
+// import middleware from 'next-auth/middleware'
+// export default middleware;
+//the below code is the same the two above lines
+
+export { default } from 'next-auth/middleware';
+
+// export function middleware(request: NextRequest) {
+//     return NextResponse.redirect(new URL('/new-page', request.url));
+// }
+
+export const config = {
+    // * zero or more parameters
+    // + one or more parameters
+    // ? zero or one parameters
+    matcher: ['/users/:id*']
+    // matcher: ['/users/:id+']
+    // matcher: ['/users/:id?']
+}
