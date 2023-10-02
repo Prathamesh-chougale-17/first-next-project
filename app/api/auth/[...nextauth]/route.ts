cfa757189b64a98745b98ad91953355abc9373b8
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-10-02 10:11:52 +0530

    saving the final changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,44 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/prisma/client";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
-export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "email", placeholder: "Email" },
-                password: { label: "Password", type: "password", placeholder: "Password" },
-            },
-            async authorize(credentials, req) {
-                if (!credentials?.email || !credentials.password) return null
-                const user = await prisma.user.findUnique({
-                    where: {
-                        email: credentials.email
-                    }
-                });
-                if (!user) return null
+import NextAuth from "next-auth"
+import { authOptions } from "../authOptions";
 
-                const passwordMatched = await bcrypt.compare(
-                    credentials.password,
-                    user.hashedPassword!
-                );
-
-                return passwordMatched ? user : null
-            }
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        })
-    ],
-    session: {
-        strategy: 'jwt'
-    }
-};
 
 const handler = NextAuth(authOptions);
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
 import Image from "next/image";
 import Pheonix from "@/public/pheonix.png";
+import { authOptions } from "./api/auth/authOptions";
+import { getServerSession } from "next-auth";
 
 interface User {
   userId: number;
@@ -10,30 +10,30 @@ interface User {
 }
 
 const Home = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    next: { revalidate: 10 },
-  });
+  // const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  //   next: { revalidate: 10 },
+  // });
   //cache: "no-store", //This is to prevent caching and always fetch the latest data and mostly used when data is dynamic
   //This is to revalidate the data after 10 seconds
   //caching memory is only valid for fetch function and not for axios and any other library
   //when we render fetch function the default caching is stored in memory and when we refresh the Home the data is fetched from memory and not from server which lead to not getting the latest data so disable the caching when data is dyanmic
-  const data: User[] = await res.json();
+  // const data: User[] = await res.json();
 
   //auth
-  // const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
   return (
     <>
-      {/* <h1>hello {session?.user?.name}</h1> */}
+      <h1>hello {session?.user?.name}</h1>
 
       <Image src={Pheonix} alt="Pheonix" />
       {/* <p>{new Date().toLocaleTimeString()}</p> */}
-      <ul>
+      {/* <ul>
         {data.map((user) => (
           <li key={user.id}>
             {user.userId} {user.title}
           </li>
         ))}
-      </ul>
+      </ul> */}
     </>
   );
 };
