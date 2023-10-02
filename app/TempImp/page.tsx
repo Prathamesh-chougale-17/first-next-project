58e0d0a78ad23464869856f864cfb9a5ae5b1839
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-10-02 09:46:48 +0530

    some minor changes

--- app/TempImp/page.tsx
+++ app/TempImp/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ProductCard from "../components/productcard/ProductCard";
 
-const page = () => {
+const TempImp = () => {
   return (
     <div className="p-2 my-2 bg-sky-400 text-xl text-yellow-300 hover:bg-sky-700">
       <ProductCard />
@@ -9,4 +9,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default TempImp;
